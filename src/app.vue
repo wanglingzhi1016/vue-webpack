50e8f2b6cc739241066e96ea510a8f71f7e33d9f
wanglingzhi <Lingzhi.Wang@fuchuang-auto.com>
2020-12-10 17:27:42 +0800

    feat: 打包less文件

--- src/app.vue
+++ src/app.vue
@@ -5,12 +5,16 @@
 <script>
 // import img from './assets/images/bg.jpg'
 // import './assets/images/small.png'
-import './assets/styles/index.css'
+import './assets/styles/index.less'
 
 export default {
   name: 'App'
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+body div {
+  font-size: 40px;
+  color: blue;
+}
 </style>
